3e35a17dd133b6d65f4adddaa5c68008203340de
sura-z <169043346+sura-z@users.noreply.github.com>
2024-05-08 15:15:31 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,18 @@
+my_string = input("Как тебя зовут?: ")
+print(len(my_string))
+print("Приветствую тебя,",(my_string).upper())
+print("Приветствую тебя,",(my_string).lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
+
+
+
+
